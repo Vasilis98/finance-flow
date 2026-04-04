da813b61d91b21b3a54e0ddff4bd991b7c2b9c56
Vasilis <billnick@hotmail.gr>
2026-04-04 13:41:29 +0300

    feat: add utility types, union types and transaction category

--- src/models.ts
+++ src/models.ts
@@ -1,8 +1,15 @@
-export interface  Transaction {
+export type TransactionCategory =
+    'Food' |
+    'Income' |
+    'Entertainment' |
+    'Utilities' |
+    'Transport';
+
+export interface Transaction {
     id: number;
     description: string;
     amount: number;
-    category: string;
+    category: TransactionCategory;
     date: string;
     isIncome: boolean;
 }
@@ -19,3 +26,9 @@ export interface Budget {
     limit: number;
     spent: number;
 }
+
+// Utility Types
+export type NewTransaction = Omit<Transaction, 'id'>;
+export type UpdateTransaction = Partial<Transaction>;
+export type TransactionSummary = Pick<Transaction, 'description' | 'amount' | 'isIncome'>;
+export type UpdateBudget = Pick<Budget, 'limit'>;
